fed28917f5202c7265d627c618762668e1e888f1
GabrielMM182 <gmm_morais@hotmail.com>
2024-12-20 18:27:03 -0300

    feat: create transaction and  debit the wallet

--- src/main/java/com/example/picpay_desafio/transaction/TransactionService.java
+++ src/main/java/com/example/picpay_desafio/transaction/TransactionService.java
@@ -10,4 +10,19 @@ public class TransactionService {
         this.transactionRepository = transactionRepository;
         this.walletRepository = walletRepository;
     }
+
+    public Transaction create(Transaction transaction) {
+        // 1 - validate
+
+        // 2 - create transaction
+       var newTransaction =  transactionRepository.save(transaction);
+
+        // 3 - debit the wallet
+        var wallet = walletRepository.findById(transaction.payer()).get();
+        walletRepository.save(wallet.debit(transaction.value()));
+
+        // 4 - call external apis like notify and verify
+
+        return newTransaction;
+    }
 }
